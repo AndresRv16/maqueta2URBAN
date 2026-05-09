a183a279151369e21ab14ae4907b8dc443617fc8
andresruiz <aruizfe1@cesde.net>
2026-05-09 10:37:27 -0500

    enlazamiento correctivo

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import AppRouter from './router/appRouter';
-import Productos from './components/Productos';
-import Contacto from './components/Contacto';
 
 function App() {
   return (
